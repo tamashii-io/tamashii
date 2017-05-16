e13a3de5ce5d2b7af96b7bf13d19b0fdfef9b131
蒼時弦也 <elct9620@frost.tw>
2017-05-16 11:01:53 +0800

    Fix syntax

--- spec/tamashii/server/connection/stream_event_loop_spec.rb
+++ spec/tamashii/server/connection/stream_event_loop_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 RSpec.describe Tamashii::Server::Connection::StreamEventLoop do
@@ -6,7 +8,7 @@ RSpec.describe Tamashii::Server::Connection::StreamEventLoop do
   let(:stream) { instance_double(Tamashii::Server::Connection::Stream) }
   let(:monitor) { instance_double(NIO::Monitor) }
 
-  context "mocked thread" do
+  context 'mocked thread' do
     before do
       allow(NIO::Selector).to receive(:new).and_return(nio)
       allow(Thread).to receive(:new) { |&block| block.call }
@@ -24,34 +26,33 @@ RSpec.describe Tamashii::Server::Connection::StreamEventLoop do
       allow(tcp_socket).to receive(:read_nonblock).with(4096, exception: false).and_return([])
       allow(tcp_socket).to receive(:close)
 
-      allow(stream).to receive(:receive).with(anything())
+      allow(stream).to receive(:receive).with(anything)
       allow(stream).to receive(:flush_write_buffer)
 
       allow(subject).to receive(:stopped?).and_return(false, true)
     end
 
-    describe "#attach" do
-      it "register socket to stream" do
+    describe '#attach' do
+      it 'register socket to stream' do
         expect(nio).to receive(:register).with(tcp_socket, :r).and_return(monitor)
         subject.attach(tcp_socket, stream)
       end
     end
 
-    describe "#detach" do
-      it "deregister socket from stream" do
+    describe '#detach' do
+      it 'deregister socket from stream' do
         expect(nio).to receive(:deregister).with(tcp_socket)
         subject.detach(tcp_socket, stream)
       end
     end
   end
 
-  describe "#stop" do
-    it "wakeup stream" do
+  describe '#stop' do
+    it 'wakeup stream' do
       allow(Thread).to receive(:new) {}
       expect(subject).to receive(:wakeup)
       subject.attach(tcp_socket, stream)
       subject.stop
     end
   end
-
 end
